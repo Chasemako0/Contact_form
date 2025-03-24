3b0fe3a10de6f006a14383af208dce662dbc90d7
Chase Mako <makochased@gmail.com>
2025-03-24 15:26:29 +0000

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -1,1 +1,14 @@
+<?php
 
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="college_data";
+
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die("Connection Failed".$conn->connect_error);
+}else{
+//    echo "Connection Successful";
+}
